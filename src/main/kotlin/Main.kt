ee7f54882fdca538bcaa8f4a816aaa39186b1ae0
gabriel <fernandeswb7@hotmail.com>
2024-05-19 19:36:26 -0300

    start

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,25 @@
 package br.com.gabriel
 
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
+
+
 fun main() {
 
+    val endpoint = "https://www.cheapshark.com/api/1.0/games"
+    val idGame = 146
 
+    val client: HttpClient = HttpClient.newHttpClient()
+
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("$endpoint?id=$idGame"))
+        .build()
+
+    val response = client
+        .send(request, BodyHandlers.ofString())
+
+    println(response.body())
 
 }
